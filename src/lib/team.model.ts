d44904e3ed0c3e20aa4c6582a0b45d584b1922ae
Adam Hlavacek <git@adamhlavacek.com>
2024-07-27 13:50:55 +0200

    Initial commit

---
+++ src/lib/team.model.ts
@@ -0,0 +1,4 @@
+export interface TeamMember {
+	github: string;
+	web?: string;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<link rel='stylesheet' href='//cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack-subset.css'>
+
+<svelte:head>
+	<title>QR Games Studio</title>
+</svelte:head>
+
+<style>
+	h1 {
+		font-size: 2em;
+	}
+	h2 {
+		font-size: 1.5em;
+	}
+	p {
+		font-size: 1.2em;
+	}
+	ul {
+		font-size: 1.2em;
+		padding-left: 3ex;
+	}
+	li {
+		list-style-type: '- ';
+	}
+	:root {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			background: #000;
+			color: #0f0;
+			font-family: "Hack", monospace;
+			margin: 8px 16px;
+
+			& > * {
+				margin-left: 0;
+				padding-left: 0;
+			}
+	}
+</style>
+
+<script>
+	/** @type {Array<import('$lib').TeamMember>} */
+	const people = [{
+		github: 'StastnyJ'
+	}, {
+		github: 'esoadamo',
+		web: "https://adamhlavacek.com"
+	}];
+
+	import manifest from '$lib/manifest.json';
+	const games = Object.values(manifest);
+</script>
+
+<h1>QR Games Studio</h1>
+
+<p>Creating open-source games that fit into a single QR code</p>
+
+<h2>Team</h2>
+<ul>
+	{#each people as person}
+		<li><a href="https://github.com/{person.github}">{person.github}</a>
+			{#if person.web}
+				<a href="{person.web}">[web]</a>
+			{/if}
+		</li>
+	{/each}
+</ul>
+
+<h2>Games</h2>
+<ul>
+	{#each games as game}
+		<li>
+			{game.name} (v{game.version}) <a href="{game.url}">[play]</a>
+		</li>
+	{/each}
+</ul>
